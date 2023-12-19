593db2268bb2d1fb804225f6c5554b15466b582c
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2023-12-19 11:08:06 -0300

    feat: read and unmarshal input

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+)
+
+type SecretRequest struct {
+	Secrets []string
+	Version string
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	input, err := io.ReadAll(os.Stdin)
+
+	if err != nil {
+		panic(fmt.Errorf("cannot read input: %w", err))
+	}
+
+	req := new(SecretRequest)
+	err = json.Unmarshal(input, req)
+
+	if err != nil {
+		panic(fmt.Errorf("cannot unmarshall input: %w", err))
+	}
+
+	var res string
+	switch req.Version {
+	default:
+		err = fmt.Errorf("unknown payload version %s", req.Version)
+	}
+
+	if err != nil {
+		panic(fmt.Errorf("cannot fetch secrets: %w", err))
+	}
+
+	fmt.Println(res)
 }
